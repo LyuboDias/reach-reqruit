dd8271875365cd27cea0b2c0dda54a0b3c315af1
Lyubomir Dias <lyubomirdias@gmail.com>
2021-07-13 10:59:08 +0100

    initial commit

---
+++ src/04-sort-array/index.js
@@ -0,0 +1,15 @@
+/**
+ * LEVEL 1: Implement using Array.prototype.sort method and NPM packages (e.g.: check-types)
+ *   - REQUIRED
+ * LEVEL 2: Implement without using Array.prototype.sort method or libraries, without regard to efficiency
+ *   - Not required, but bonus points awarded
+ * LEVEL 3: Implement without using Array.prototype.sort method or libraries, efficiently
+ *   - Not required, but major bonus points awarded
+ */
+
+// Average and worst case time complexity (Big-O) of my function is:
+// Explain:
+
+module.exports = function sortArray(arr) {
+  // TODO
+};
